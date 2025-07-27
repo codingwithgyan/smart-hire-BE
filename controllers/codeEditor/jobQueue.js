3a28f3ba257649be439b5627b81be603cfd0e5e8
Gyaneshwar Kumar <codewithgyan@gmail.com>
2025-07-28 00:35:53 +0530

    Compiler job fix
    Compiler job fix

--- controllers/codeEditor/jobQueue.js
+++ controllers/codeEditor/jobQueue.js
@@ -1,49 +1,46 @@
-const Queue = require('bull');
+const Queue = require("bull");
 const Job = require("../../models/job.model");
-const runPythonFile = require('./ExecutePython');
+const runPythonFile = require("./ExecutePython");
 const jobQueue = new Queue("job-queue");
-const  { JOB_STATUS, LANGUAGE_SUPPORT} = require("../../constants/index");
+const { JOB_STATUS, LANGUAGE_SUPPORT } = require("../../constants/index");
 
-const {PENDING, SUCCESS, ERROR} = JOB_STATUS;
-const {JAVASCRIPT, PYTHON} = LANGUAGE_SUPPORT;
+const { PENDING, SUCCESS, ERROR } = JOB_STATUS;
+const { JAVASCRIPT, PYTHON } = LANGUAGE_SUPPORT;
 const TOTAL_WORKERS = 8;
 
-jobQueue.process(TOTAL_WORKERS,async({data})=>{
-    const { jobId } = data;
-    const job = await Job.findById(jobId);
-    if(!job)
-        throw "Job details not found"
+jobQueue.process(TOTAL_WORKERS, async ({ data }) => {
+  console.log("==============job started==============", data);
+  const { jobId } = data;
+  const job = await Job.findById(jobId);
+  if (!job) throw "Job details not found";
 
-    try
-    {
-        job["startedAt"] = new Date();
-        const codeResponse = await runPythonFile(job?.filePath,job?.lang);
+  try {
+    job["startedAt"] = new Date();
+    const codeResponse = await runPythonFile(job?.filePath, job?.lang);
 
-        job["completedAt"] = new Date();
-        job["status"] = SUCCESS;
-        job["output"] = codeResponse;
-        
-        await job.save();
-    }
-    catch(error)
-    {
-        job["completedAt"] = new Date();
-        job["status"] = ERROR;
-        job["output"] = JSON.stringify(error);
-        await job.save();
+    job["completedAt"] = new Date();
+    job["status"] = SUCCESS;
+    job["output"] = codeResponse;
 
-        return res.status(400).send(
-            {
-                status:false,
-                error:JSON.stringify(error?.stderr)
-            }
-        );
-    }
-    return true;
+    await job.save();
+  } catch (error) {
+    console.log("==============err started==============", error);
+    job["completedAt"] = new Date();
+    job["status"] = ERROR;
+    job["output"] = JSON.stringify(error);
+    await job.save();
+
+    return res.status(400).send({
+      status: false,
+      error: JSON.stringify(error?.stderr),
+    });
+  }
+  return true;
 });
 
-const addJobToQueue = async(jobId) => {
-   await jobQueue.add({jobId})
-}
+const addJobToQueue = async (jobId) => {
+  await jobQueue.add({ jobId });
+  console.log("==============jobQueue==============", jobQueue[jobId]);
+};
 
-module.exports = {addJobToQueue};
+module.exports = { addJobToQueue };
