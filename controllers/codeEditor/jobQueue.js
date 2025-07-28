3968ca9943c9e16bd238ecbfac49734e7298fc77
Gyaneshwar Kumar <codewithgyan@gmail.com>
2025-07-28 21:33:36 +0530

    job queue error fix

--- controllers/codeEditor/jobQueue.js
+++ controllers/codeEditor/jobQueue.js
@@ -1,46 +1,47 @@
 const Queue = require("bull");
 const Job = require("../../models/job.model");
 const runPythonFile = require("./ExecutePython");
-const jobQueue = new Queue("job-queue");
-const { JOB_STATUS, LANGUAGE_SUPPORT } = require("../../constants/index");
 
+const redisConfig = {
+  host: process.env.REDIS_HOST || "localhost",
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || "123456",
+};
+
+const jobQueue = new Queue("job-queue", redisConfig);
+const { JOB_STATUS, LANGUAGE_SUPPORT } = require("../../constants/index");
 const { PENDING, SUCCESS, ERROR } = JOB_STATUS;
 const { JAVASCRIPT, PYTHON } = LANGUAGE_SUPPORT;
-const TOTAL_WORKERS = 8;
+const TOTAL_WORKERS = 4;
 
 jobQueue.process(TOTAL_WORKERS, async ({ data }) => {
-  console.log("==============job started==============", data);
   const { jobId } = data;
   const job = await Job.findById(jobId);
-  if (!job) throw "Job details not found";
 
   try {
-    job["startedAt"] = new Date();
-    const codeResponse = await runPythonFile(job?.filePath, job?.lang);
-
-    job["completedAt"] = new Date();
-    job["status"] = SUCCESS;
-    job["output"] = codeResponse;
-
-    await job.save();
+    job.startedAt = new Date();
+    const codeResponse = await runPythonFile(job.filePath, job.lang);
+    job.completedAt = new Date();
+    job.status = SUCCESS;
+    job.output = codeResponse;
   } catch (error) {
-    console.log("==============err started==============", error);
-    job["completedAt"] = new Date();
-    job["status"] = ERROR;
-    job["output"] = JSON.stringify(error);
-    await job.save();
-
-    return res.status(400).send({
-      status: false,
-      error: JSON.stringify(error?.stderr),
-    });
+    job.completedAt = new Date();
+    job.status = ERROR;
+    job.output = JSON.stringify(error);
   }
+
+  await job.save();
   return true;
 });
 
 const addJobToQueue = async (jobId) => {
-  await jobQueue.add({ jobId });
-  console.log("==============jobQueue==============", jobQueue[jobId]);
+  try {
+    if (jobQueue.hasOwnProperty("jobId")) {
+      jobQueue["jobId"] = "";
+    }
+    await jobQueue.add({ jobId });
+  } catch (error) {
+    throw new Error("Failed to add job to queue");
+  }
 };
-
 module.exports = { addJobToQueue };
